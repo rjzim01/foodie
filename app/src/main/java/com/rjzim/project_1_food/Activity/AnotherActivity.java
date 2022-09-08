2b183ae579a0dab265a2a849ac29bbd9c4dc1b18
rjzim01 <rjzim01@gmail.com>
2022-09-08 22:52:56 +0600

    Commit_7_test

--- app/src/main/java/com/rjzim/project_1_food/Activity/AnotherActivity.java
+++ app/src/main/java/com/rjzim/project_1_food/Activity/AnotherActivity.java
@@ -3,21 +3,21 @@ package com.rjzim.project_1_food.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.rjzim.project_1_food.R;
 
-public class DetailActivity extends AppCompatActivity {
+public class AnotherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
-        System.out.println("hello");
+        setContentView(R.layout.activity_another);
+
+        //System.out.println("hello");
+
+        TextView textView = findViewById(R.id.textView12);
+
+        textView.setText("hello");
     }
 }
-
-/*
-*
-* jgvhjvhjvjhvjhvjfyjufy
-* jgfghvhjvhjvjk
-* jhvhjv*/
