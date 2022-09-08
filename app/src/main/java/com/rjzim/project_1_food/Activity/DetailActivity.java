5fb55d6da5b1354995ce9f5026736887e16c21a1
rjzim01 <rjzim01@gmail.com>
2022-09-08 22:23:01 +0600

    commit_6

---
+++ app/src/main/java/com/rjzim/project_1_food/Activity/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.rjzim.project_1_food.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rjzim.project_1_food.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
